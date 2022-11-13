961361d081358367372c00947e210b6828e30bfb
Sohail1011 <aratavalga@gmail.com>
2022-11-12 19:21:39 -0500

    for practice

---
+++ NetCore.Data/Repositories/CarRepository.cs
@@ -0,0 +1,86 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using NetCore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCore.Data.Repositories
+{
+    public class CarRepository : ICarRepository
+    {
+        private readonly MySQLConfiguration _connectionString;
+        public CarRepository(MySQLConfiguration connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected MySqlConnection dbConnection()
+        {
+            return new MySqlConnection(_connectionString.ConnectionString);
+        }
+
+        public async Task<bool> DeleteCar(Car car)
+        {
+            var db = dbConnection();
+
+            var sql = @"DELETE FROM carros WHERE id = @Id ";
+
+            var result = await db.ExecuteAsync(sql, new { Id = car.Id });
+
+            return result > 0;
+        }
+
+        public async Task<IEnumerable<Car>> GetAllCars()
+        {
+            var db = dbConnection();
+
+            var sql = @" SELECT id, make, model, color, year, doors
+                        FROM carros";
+
+            return await db.QueryAsync<Car>(sql, new { });
+        }
+
+        public async Task<Car> GetDetails(int id)
+        {
+            var db = dbConnection();
+
+            var sql = @" SELECT id, make, model, color, year, doors
+                        FROM carros 
+                        WHERE id = @Id ";
+
+            return await db.QueryFirstOrDefaultAsync<Car>(sql, new { Id = id });
+        }
+
+        public async Task<bool> InsertCar(Car car)
+        {
+            var db = dbConnection();
+
+            var sql = @" INSERT INTO carros(make, model, color, year, doors)
+                        VALUES(@Make, @Model, @Color, @Year, @Doors) ";
+
+            var result = await db.ExecuteAsync(sql, new { car.Make, car.Model, car.Color, car.Year, car.Doors });
+            return result > 0;
+        }
+
+        public async Task<bool> UpdateCar(Car car)
+        {
+            var db = dbConnection();
+
+            var sql = @" UPDATE carros
+                        SET make = @Make, 
+                            model = @Model, 
+                            color = @Color, 
+                            year = @Year, 
+                            doors = @Doors 
+                        WHERE id  = @Id";
+
+            var result = await db.ExecuteAsync(sql, new
+            { car.Make, car.Model, car.Color, car.Year, car.Doors, car.Id });
+
+            return result > 0;
+        }
+    }
+}
---
+++ NetCore/Controllers/CarsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCore.Data.Repositories;
+using NetCore.Model;
+
+namespace NetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        private readonly ICarRepository _carRepository;
+
+        public CarsController(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCars()
+        {
+            return Ok(await _carRepository.GetAllCars());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCarDetails(int id)
+        {
+            return Ok(await _carRepository.GetDetails(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCar([FromBody] Car car)
+        {
+            if (car == null)
+                return BadRequest();
+
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+
+            var created = await _carRepository.InsertCar(car);
+
+            return Created("Created", created);
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCar([FromBody] Car car)
+        {
+            if (car == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _carRepository.UpdateCar(car);
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCar(int id)
+        {
+            await _carRepository.DeleteCar(new Car { Id = id });
+
+            return NoContent();
+        }
+    }
+}
